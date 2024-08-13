6f72c8ae7ec6838f1690b25424018a3d3d74ac9b
Soroibumi <157578772+Soroibumi@users.noreply.github.com>
2024-08-13 18:19:45 +0900

    Add files via upload

---
+++ _next/static/EDGknBY1NLRuc3HQCZcmk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c5b9c56cd19867a6.css","static/chunks/pages/index-b12a3199070fd854.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
